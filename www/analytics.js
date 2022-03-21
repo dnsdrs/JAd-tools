8a5e540f8a1f98f845d0ea360e969d4f303d014d
dnsdrs <76854122+dnsdrs@users.noreply.github.com>
2022-03-21 19:00:34 +0100

    analytics avec un gros js qui fait tout

---
+++ www/analytics.js
@@ -0,0 +1,13 @@
+var s = document.createElement("script");
+s.async = true;
+s.src = "https://www.googletagmanager.com/gtag/js?id=G-7BG3QSNNKJ";
+s.setAttribute("importance", "high");
+var g = document.getElementsByTagName("head")[0];
+g.parentNode.insertBefore(s, g);
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-7BG3QSNNKJ');
